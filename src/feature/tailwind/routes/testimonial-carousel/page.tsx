37334f37f061834d828b66e4f377d8f276329ec3
ashik-75 <ashikex49@gmail.com>
2024-01-08 23:19:09 +0600

    feat:upgrade thisngs

--- src/feature/tailwind/routes/testimonial-carousel/page.tsx
+++ src/feature/tailwind/routes/testimonial-carousel/page.tsx
@@ -5,7 +5,7 @@ const TestimonalCarousel: React.FC = () => {
   return (
     <div className="flex h-full items-center overflow-hidden">
       <div className="animate-marquee flex min-w-full shrink-0 gap-10">
-        {Array.from({ length: 10 }).map((item, i) => (
+        {Array.from({ length: 10 }).map((_, i) => (
           <div key={i + 1} className="shrink-0">
             <Testimonials />
           </div>
